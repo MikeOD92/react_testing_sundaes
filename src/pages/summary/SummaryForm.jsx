7986ca4ad164d4e254f64b0554db093cec8d239f
Michael <michaelodell660@gmail.com>
2022-02-28 18:03:47 -0800

    set up inital conditions and checkbox enables button tests

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Popover } from "react-bootstrap";
 
 export default function SummaryForm() {
   const [accept, setAccept] = useState(false);
--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,7 +1,8 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import SummaryForm from "../SummaryForm";
 
-test("checkbox enables button", () => {
+test("inital conditions:", () => {
   render(<SummaryForm />);
   const checkbox = screen.getByRole("checkbox", {
     name: /terms and conditions/i,
@@ -10,10 +11,24 @@ test("checkbox enables button", () => {
 
   expect(checkbox).not.toBeChecked();
   expect(button).toBeDisabled();
+});
 
-  fireEvent.click(checkbox);
+test("checkbox enables button", () => {
+  render(<SummaryForm />);
+  const checkbox = screen.getByRole("checkbox", {
+    name: /terms and conditions/i,
+  });
+  const button = screen.getByRole("button", { name: /accept/i });
+
+  userEvent.click(checkbox);
   expect(button).toBeEnabled();
 
-  fireEvent.click(checkbox);
+  userEvent.click(checkbox);
   expect(button).toBeDisabled();
+});
+
+test("popover responseds to hover", () => {
+  // pop over starts out hidden
+  // popover appears upone mouse over of checkbox label
+  // popover disappears when we mouse out
 });
