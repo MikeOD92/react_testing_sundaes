f4171a3c29ecb1dbbe6949a71b608ef7b3297b92
Michael <michaelodell660@gmail.com>
2022-03-03 11:56:59 -0800

    building usecontext hook and provider compoent

---
+++ src/context/OrderDetails.jsx
@@ -0,0 +1,62 @@
+import { createContext, useContext, useEffect, useState, useMemo } from "react";
+import pricePerItem from "../constants/index";
+const OrderDetails = createContext();
+
+// custom hook to check if we are inside provider
+export function useOrderDetails() {
+  const context = useContext(OrderDetails);
+
+  if (!context) {
+    throw new Error("useOrderDetails must be inise a OrderDetailsProvider");
+  }
+  return context;
+}
+
+function calculateSubtotal(optionType, optionCounts) {
+  let optionCount = 0;
+  for (const count of optionCounts[optionType].values()) {
+    optionCount += count;
+  }
+
+  return optionCount * pricePerItem[optionType];
+}
+
+export function OrderDetailsProvider(props) {
+  const [optionCounts, setOptionCounts] = useState({
+    scoops: new Map(),
+    toppings: new Map(),
+  });
+  const [totals, setTotal] = useState({
+    scoops: 0,
+    toppings: 0,
+    grandTotal: 0,
+  });
+
+  useEffect(() => {
+    const scoopsSubtotal = calculateSubtotal("scoops", optionCounts);
+    const toppingSubtotal = calculateSubtotal("toppings", optionCounts);
+    const grandTotal = scoopsSubtotal + toppingSubtotal;
+    setTotal({
+      scoops: scoopsSubtotal,
+      toppings: toppingSubtotal,
+      grandTotal,
+    });
+  }, [optionCounts]);
+
+  const value = useMemo(() => {
+    function updateItemCount(itemName, newItemCount, optionType) {
+      const newOptionCounts = { ...optionCounts };
+
+      // update option count for this item with new val
+
+      const optionCountsMap = optionCounts[optionType];
+      optionCountsMap.set(itemName, parseInt(newItemCount));
+
+      setOptionCounts(newOptionCounts);
+    }
+    // getter : obj containing details, scoops, toppings, subtotal and total
+    // setter : update option counts
+    return [{ ...optionCounts, totals }, updateItemCount];
+  }, [optionCounts, totals]);
+  return <OrderDetails.Provider value={value} {...props} />;
+}
---
+++ src/pages/entry/tests/totalUpdates.test.jsx
@@ -0,0 +1,29 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Options from "../Options";
+
+test("update scrool subtotal when scoop changes", async () => {
+  render(<Options optionType="scoops" />);
+
+  // make sure total starts at 0.00
+  const scoopsSubtotal = screen.getByText("Scoops total: $", { exact: false });
+
+  expect(scoopsSubtotal).toHaveTextContent("0.00");
+  // update vanilla scoops to 1 and check subtotal
+  const vanillaInput = await screen.findByRole("spinbutton", {
+    name: "Vanilla",
+  });
+
+  userEvent.clear(vanillaInput);
+  userEvent.type(vanillaInput, "1");
+  expect(scoopsSubtotal).toHaveTextContent("2.00");
+
+  // update chocalate scoops to 2 check substoal
+  const ChocoInput = await screen.findByRole("spinbutton", {
+    name: /Choclate/i,
+  });
+
+  userEvent.clear(ChocoInput);
+  userEvent.type(ChocoInput, "2");
+  expect(scoopsSubtotal).toHaveTextContent("6.00");
+});
